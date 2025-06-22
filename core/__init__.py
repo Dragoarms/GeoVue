9593a5bb7185fb23cda89ef4f576dbba8bb6301f
George Symonds <george.symonds@hotmail.co.uk>
2025-06-22 21:49:38 +0100

    Delete core directory

--- core/__init__.py
+++
@@ -1,8 +0,0 @@
-# core\__init__.py
-
-from core.config_manager import ConfigManager
-from core.file_manager import FileManager
-from core.repo_updater import RepoUpdater
-from core.tesseract_manager import TesseractManager
-from core.translator import TranslationManager
-from core.visualization_manager import VisualizationManager
